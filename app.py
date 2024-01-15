3f7d365b0fc91bbd1df9be537eeeb2de271294b8
Shreyash Jagtap <shreyash5522@gmail.com>
2024-01-15 17:59:17 +0530

    15-Jan-2024 | Database Connection Set

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/databasename'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    phone_num = db.Column(db.String(120), unique=False, nullable=False)
+    msg = db.Column(db.String(120), unique=True, nullable=False)
+    date = db.Column(db.String(120), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
 
 @app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html')
 
+
 @app.route('/post')
 @app.route('/post.html')
 def post():
     return render_template('post.html')
+
 
 @app.route('/about')
 @app.route('/about.html')
@@ -18,9 +34,20 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 @app.route('/contact.html')
 def contact():
+    if (request.method == "POST"):
+
+        '''Add entry to database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contacts(name=name, phone_num=phone, msg=message, email=email)
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contact.html')
 
 
